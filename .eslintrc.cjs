eecf7719b6c66047fa463f1a8c8187654f4acf20
Felix Gohla <felix.gohla@student.hpi.de>
2023-05-07 19:01:59 +0200

    Setup Linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,26 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
+  settings: {
+    'import/resolver': {
+      'node': {
+        'extensions': ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  },
   rules: {
     'react-refresh/only-export-components': 'warn',
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'no-trailing-spaces': 'error',
+    quotes: ['error', 'single'],
+    'no-console': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'warn', // Consider using explicit annotations for object literals and function return types even when they can be inferred.
+    'no-empty': 'warn',
   },
 }
